8bf955f678d8473768e529e795e73460445a57e9
Guilherme <gwoliveira@gmail.com>
2016-11-30 19:54:33 -0200

    iniciando start.spring.io

---
+++ src/main/java/br/edu/facpoli/RandomuserApplication.java
@@ -0,0 +1,12 @@
+package br.edu.facpoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomuserApplication.class, args);
+	}
+}
