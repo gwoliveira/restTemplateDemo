b7785c12390ab73281d86dd1dd643f526f6e6f3a
Guilherme Willian de Oliveira <gwoliveira@gmail.com>
2016-11-30 23:03:55 -0200

    restTemplate

--- src/main/java/br/edu/facpoli/controller/RandomUserController.java
+++ src/main/java/br/edu/facpoli/controller/RandomUserController.java
@@ -1,16 +1,10 @@
 package br.edu.facpoli.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
-import br.edu.facpoli.pojo.Login;
-import br.edu.facpoli.pojo.Name;
-import br.edu.facpoli.pojo.Picture;
-import br.edu.facpoli.pojo.RandomUser;
 import br.edu.facpoli.pojo.RandomUserResponse;
 
 @Controller
@@ -22,29 +16,7 @@ public class RandomUserController {
 	}
 
 	private RandomUserResponse generateRandomUserResponse() {
-		List<RandomUser> randomUsers = new ArrayList<>();
-		randomUsers.add(generateRandomUser("ms","Sara","Cox"));
-		randomUsers.add(generateRandomUser("ms","Maria","Rosa"));
-		
-		
-		RandomUserResponse randomUserResponse = new RandomUserResponse();
-		randomUserResponse.setRandomUsers(randomUsers);
-		return randomUserResponse;
+		return new RestTemplate().getForObject("https://randomuser.me/api/?results=5&nat=br", RandomUserResponse.class);
 	}
 
-	private RandomUser generateRandomUser(String title, String first, String last) {
-		RandomUser randomUser = new RandomUser();
-		randomUser.setName(new Name(title, first, last));
-		randomUser.setLogin(new Login());
-		randomUser.setPhone("(400)-321-1962");
-		randomUser.setEmail("sara.cox47@example.com");
-		randomUser.setNat("7/2/1970");
-		
-		Picture picture = new Picture();
-		picture.setLarge("https://randomuser.me/api/portraits/women/39.jpg");
-		picture.setMedium("https://randomuser.me/api/portraits/med/women/39.jpg");
-		picture.setThumbnail("https://randomuser.me/api/portraits/thumb/women/39.jpg");
-		randomUser.setPicture(picture);
-		return randomUser;
-	}
 }
